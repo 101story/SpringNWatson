85355e72eb55a47f4227c33a6f4b16b9a068a7fa
101story <hooraystory@gmail.com>
2017-11-01 18:33:55 +0900

    subway bot

--- SubwayBotMiniproj/src/main/java/net/n1books/chat/dev/WatsonSay.java
+++ SubwayBotMiniproj/src/main/java/net/n1books/chat/dev/WatsonSay.java
@@ -1,12 +1,19 @@
 package net.n1books.chat.dev;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +26,7 @@ import com.ibm.watson.developer_cloud.conversation.v1.model.InputData;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageOptions;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 
+// @RestController : view 로 가지 않고 json 형식으로 넘김 
 @RestController
 public class WatsonSay {
 	private static Logger logger = LoggerFactory.getLogger(WatsonSay.class);
@@ -28,58 +36,69 @@ public class WatsonSay {
 	private String password;
 	@Value("${conversation.workspaceId}")
 	private String workspaceId;
-	
-	@RequestMapping(value="watsonsay")
-	public MessageResponse watsonsay(String isay, HttpSession session) {
+
+	@RequestMapping(value = "watsonsay")
+	public MessageResponse watsonsay(String isay, Model model, HttpSession session) {
 		logger.info("user input : " + isay);
-		
+		MessageResponse response = null;
+
 		Conversation service = new Conversation(Conversation.VERSION_DATE_2017_05_26);
 		service.setUsernameAndPassword(username, password);
 
-		MessageResponse response = null;
 		Context context = (Context) session.getAttribute("context");
+		logger.info("context: " + context);
+
 		MessageOptions options = null;
 		String msg = isay;
-		StringBuffer watsonSay = null;
-		
-		options = new MessageOptions.Builder()
-			    .workspaceId(workspaceId)
-			    .input(new InputData.Builder(msg).build())
-			    .context(context)
-			    .build();
+
+		options = new MessageOptions.Builder().workspaceId(workspaceId).input(new InputData.Builder(msg).build())
+				.context(context).build();
 		response = service.message(options).execute();
-		
-		watsonSay = new StringBuffer();
-		for(String text : response.getOutput().getText()) {
-			watsonSay.append(text);
-			watsonSay.append(" ");
-		}
-		
-		logger.info("Watson : " + watsonSay);
+		logger.info(response.toString());
+
+//		JSONParser parser = new JSONParser();
+//		Object obj = null;
+//		try {
+//			obj = parser.parse(response.getContext().getSystem().toString());
+//		} catch (ParseException e) {
+//			e.printStackTrace();
+//		}
+//		JSONObject jsonObj = (JSONObject) obj;
+//		JSONArray arrStack = (JSONArray) jsonObj.get("dialog_stack");
+//			
+//		String img = null;
+//		if (jsonObj != null) {
+//			JSONObject jsonNode = (JSONObject) arrStack.get(0);
+//			String node = (String) jsonNode.get("dialog_node");
+//			logger.info("Node : "+node);
+//			if(node.equals("node_9_1508721360692")) {
+//				img = "menusm.png";
+//				model.addAttribute("imgName",img);
+//				logger.info("imgName : "+img);
+//			}
+//		}
 		
 		context = response.getContext();
 		session.setAttribute("context", context);
 		return response;
 	}
-	
-	
-	@RequestMapping(value="/keyboard", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/keyboard", method = RequestMethod.GET)
 	public String kakaoChat() {
 		logger.info("keyboard");
-		
+
 		JSONObject jobjBtn = new JSONObject();
 		jobjBtn.put("type", "text");
-		
+
 		return jobjBtn.toJSONString();
 	}
-	
-	
-	@RequestMapping(value="/message", method=RequestMethod.POST, headers="Accept=application/json;charset=UTF-8",
-			produces= {MediaType.APPLICATION_JSON_UTF8_VALUE})
+
+	@RequestMapping(value = "/message", method = RequestMethod.POST, headers = "Accept=application/json;charset=UTF-8", produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
 	@ResponseBody
 	public String kakaoChatMsg(@RequestBody JSONObject resObj, HttpSession session) {
 		logger.info("message");
-		
+
 		Conversation service = new Conversation(Conversation.VERSION_DATE_2017_05_26);
 		service.setUsernameAndPassword(username, password);
 
@@ -87,35 +106,57 @@ public class WatsonSay {
 		Context context = (Context) session.getAttribute("context");
 		MessageOptions options = null;
 		StringBuffer watsonSay = null;
-		
+
 		String content;
-        content = (String) resObj.get("content");
-		
-		options = new MessageOptions.Builder()
-			    .workspaceId(workspaceId)
-			    .input(new InputData.Builder(content).build())
-			    .context(context)
-			    .build();
+		content = (String) resObj.get("content");
+
+		options = new MessageOptions.Builder().workspaceId(workspaceId).input(new InputData.Builder(content).build())
+				.context(context).build();
 		response = service.message(options).execute();
-		
+
 		watsonSay = new StringBuffer();
-		for(String text : response.getOutput().getText()) {
+		for (String text : response.getOutput().getText()) {
 			watsonSay.append(text);
 			watsonSay.append(" ");
 		}
-		
+
 		logger.info("Watson : " + watsonSay);
-		
+
 		context = response.getContext();
 		session.setAttribute("context", context);
+
+		JSONParser parser = new JSONParser();
+		Object obj = null;
+		try {
+			obj = parser.parse(response.getContext().getSystem().toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		JSONObject jsonObj = (JSONObject) obj;
+		JSONArray arrStack = (JSONArray) jsonObj.get("dialog_stack");
+			
+		String img = null;
 		
-        JSONObject jobjRes = new JSONObject();
-        JSONObject jobjText = new JSONObject();
-        jobjText.put("text",watsonSay.toString());
-        
-        jobjRes.put("message", jobjText);
-        logger.info(jobjRes.toJSONString());
-        
-        return  jobjRes.toJSONString();
+		JSONObject jobjRes = new JSONObject();
+		JSONObject jobjText = new JSONObject();
+		jobjText.put("text", watsonSay.toString());
+		if (jsonObj != null) {
+			JSONObject jsonNode = (JSONObject) arrStack.get(0);
+			String node = (String) jsonNode.get("dialog_node");
+			logger.info("Node : "+node);
+			if(node.equals("node_9_1508721360692")) {
+				Map<String , String> photo = new HashMap<String, String>();
+				photo.put("url", "http://cfile23.uf.tistory.com/image/2665D143575D40DD0D6EFA");
+				photo.put("width", "350");
+				photo.put("height", "300");
+				jobjText.put("photo", photo);
+				logger.info("imgName : "+img);
+			}
+		}
+
+		jobjRes.put("message", jobjText);
+		logger.info(jobjRes.toJSONString());
+
+		return jobjRes.toJSONString();
 	}
 }
--- SubwayBotMiniproj/src/main/webapp/WEB-INF/views/chatbot.jsp
+++ SubwayBotMiniproj/src/main/webapp/WEB-INF/views/chatbot.jsp
@@ -1,13 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<c:url value="/resources/css/bootstrap.min.css" />">
-<link rel="stylesheet" href="<c:url value="/resources/css/bubbles.css" />">
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/bootstrap.min.css" />">
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/bubbles.css" />">
 <script src="<c:url value="/resources/js/jquery-3.2.1.js" />"></script>
 <script src="<c:url value="/resources/js/bootstrap.min.js" />"></script>
 <title>Insert title here</title>
@@ -47,9 +49,17 @@ function ajax_process(_isay){
 			var watsonsay =
 				'<p class="triangle-border left">' + data.output.text;
 				
-			if (_isay ==='') {
-				watsonsay += '<br/><img src="<c:url value="/resources/img/menusm.png"/>" width="300" />';
-			}	
+			/* var movie_name = '${imgName}';
+			console.log('${imgName}');
+			watsonsay = watsonsay + movie_name; */
+			/* '<c:set var="imgName" value="${imgName }"/>'
+			console.log(imgName); */
+			var node = data.context.system.dialog_stack[0].dialog_node;
+			if(node=="node_9_1508721360692"){
+				watsonsay += '<br/><img src="<c:url value="/resources/img/menusm.png"/>" width="400" />';
+			}else if(node=="node_3_1508719554698"){
+				watsonsay += '<br/><img src="<c:url value="/resources/img/bread.png"/>" width="400"/>';
+			}
 			
 			watsonsay += '</p>';
 			$('#said').append(watsonsay);	
@@ -60,8 +70,9 @@ function ajax_process(_isay){
 </script>
 </head>
 <body>
-<div id="said"></div>
-<input type="text" id="txt_isay" class="triangle-border center" />
-<button type="button" id="btn_isay" autofocus="autofocus" class="btn btn-default">전송</button>
+	<div id="said"></div>
+	<input type="text" id="txt_isay" class="triangle-border center" />
+	<button type="button" id="btn_isay" autofocus="autofocus"
+		class="btn btn-default">전송</button>
 </body>
 </html>
